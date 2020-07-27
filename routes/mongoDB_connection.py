ec03baf725ccbad43fe8fdee9f3b8f90936ef3e5
stevenjoeng <stevenswjoeng@gmail.com>
2020-07-27 19:50:59 +0900

    Fix some bugs

--- routes/mongoDB_connection.py
+++ routes/mongoDB_connection.py
@@ -8,23 +8,18 @@ Created on Tue Jul 21 13:13:40 2020
 
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+# import datetime
 
 import importlib.util
-from .StockInfoStorage import StockCurrentInfoStorage
-from .StockInfoStorage import StockPastInfoStorage
+from StockInfoStorage import StockCurrentInfoStorage
+from StockInfoStorage import StockPastInfoStorage
 
 """
 operation types:
     create
-
-ex) 
-obj = StockPastInfoStorage(....)
-connect(create, obj)
-or
-connect(delete_all)
 """
 
-def connect(self, operation: str, obj = None):
+def connect(operation: str, obj = None):
 
     spec = importlib.util.spec_from_file_location("accounts", "../config/accounts.py")
     accounts = importlib.util.module_from_spec(spec)
@@ -38,9 +33,9 @@ def connect(self, operation: str, obj = None):
     global collection
 
     try:
-        client = MongoClient(host='mongodb+srv://' + self.__USERNAME + ':' + self.__PASSWORD + '@cluster0-tester.cwbjo.mongodb.net/<Cluster0-Tester>?retryWrites=true&w=majority',
-                                  maxPoolSize=100, socketTimeoutMS=20000, connectTimeoutMS=20000, serverSelectionTimeoutMS=20000)
-        print(self.client)
+        client = MongoClient(host='mongodb+srv://' + _username + ':' + _password + '@cluster0-tester.cwbjo.mongodb.net/<Cluster0-Tester>?retryWrites=true&w=majority',
+                                  maxPoolSize=100, socketTimeoutMS=30000, connectTimeoutMS=30000, serverSelectionTimeoutMS=30000)
+        print(client)
     except ConnectionFailure:
         print("Connection Error")
 
@@ -63,52 +58,19 @@ def connect(self, operation: str, obj = None):
             if operation == "create":
                 create_past_info(obj)
 
-
-
-
-    db = client["test"]
-    collection = db["test"]
-
-    """
-    if operation == "deleteAll":
-        deleteAll()
-
-
-    if obj is not None:
-        if operation == "create":
-            create(obj)
-        else:
-            print("Invalid Operation, Try again - DB connection is automatically closed.")
-            client.close()
-        '''
-        elif operation == "read":
-            read(obj)
-        elif operation == "update":
-            update(obj)
-        elif operation == "delete":
-            delete(obj)
-        '''
-    """
-
-def closeConnection(self):
+def closeConnection():
     client.close()
     print("Connection closed")
 
 
 # Need to be added trading_trends later.
-def create_past_info(self, obj):
-    """
-            name: 주식 이름    (ex. SK하이닉스)
-            code: 주식 코드    (ex. 000660)
-            marketType: 증권 시장 종류 (ex. KOSPI)
-            highPrice: 고가      lowPrice: 저가       startingPrice: 시가     closingPrice: 종가
-            increasingRate: 전일비(%)      tradingVolume: 거래량      datetime: 날짜 (ex. 2019.01.01)
-        """
+def create_past_info(obj):
+
     tmp = {"code": obj.getCode(), "marketType": obj.getMarketType(),
            "highPrice": obj.getHighPrice(), "lowPrice": obj.getLowPrice(),
            "startingPrice": obj.getStartingPrice(), "closingPrice": obj.getClosingPrice(),
            "increasingRate": obj.getIncreasingRate(), "tradingVolume": obj.getTradingVolume(),
-           "datetime": obj.getDatetime()
+           "date": obj.getDatetime()
            }
 
     result = collection.insert_one(tmp)
@@ -119,7 +81,7 @@ def create_past_info(self, obj):
         print("Failure - Create")
 
 
-def create_cur_info(self, obj):
+def create_cur_info(obj):
     tmp = {"code": obj.getCode(), "marketType": obj.getMarketType(),
            "buyingPrice": obj.getBuyingPrice(), "buyingVolume": obj.getBuyingVolume(),
            "sellingPrice": obj.getSellingPrice(), "sellingVolume": obj.getSellingVolume(),
@@ -127,7 +89,7 @@ def create_cur_info(self, obj):
            "startingPrice": obj.getStartingPrice(), "closingPrice": obj.getClosingPrice(),
            "increasingRate": obj.getIncreasingRate(), "tradingVolume": obj.getTradingVolume(),
            "tradingValue": obj.getTradingValue(), "tradingTrends": obj.getTradingTrends(),
-           "datetime": obj.getDatetime()
+           "date": obj.getDatetime()
            }
     result = collection.insert_one(tmp)
 
@@ -138,9 +100,16 @@ def create_cur_info(self, obj):
 
 
 """
-def delete_all(self, col):
+def delete_all(col):
     x = col.delete_many({})
     print(x.deleted_count, " documents successfully deleted")
 
 """
+"""
+d = datetime.datetime(2019, 12, 11)
+obj = StockPastInfoStorage(
+    "12345", "KOSPI", 12.11, 12.11, 12.12, 12.12, 12.12, 12.12, d
+)
 
+connect("create", obj)
+"""
